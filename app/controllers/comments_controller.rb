5aa049a29a3abd4dac4be2b3910966ae97303254
Julián Pinzón Eslava <julian@julianpinzon.com>
2024-01-28 19:56:52 +1100

    Install turbo 8 beta 4 and configure morphing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,16 +22,8 @@ class CommentsController < ApplicationController
   # POST /comments or /comments.json
   def create
     @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to root_path, notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment.save!
+    redirect_to root_path, notice: "Comment was successfully created."
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
