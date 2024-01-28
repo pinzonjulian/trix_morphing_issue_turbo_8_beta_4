78d5c308adcc0570c696f2fa3a76e819b72d3fa3
Julián Pinzón Eslava <julian@julianpinzon.com>
2024-01-28 19:40:17 +1100

    setup model and controllers with scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_28_083356) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_28_083452) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_28_083356) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
